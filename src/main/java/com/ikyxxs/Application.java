ae47dd6c783a4e943ffca7037a82938977c39a3b
木白 <624104747@qq.com>
2018-05-29 19:09:20 +0800

    限制只能上传文件

--- src/main/java/com/ikyxxs/Application.java
+++ src/main/java/com/ikyxxs/Application.java
@@ -98,6 +98,10 @@ public class Application implements CommandLineRunner {
         File[] files = dir.listFiles();
         if (null != files && files.length > 0) {
             for (File file : files) {
+                if (!file.isFile()) {
+                    continue;
+                }
+
                 Book book;
                 if (bookMap.containsKey(file.getName())) {
                     book = bookMap.get(file.getName());
